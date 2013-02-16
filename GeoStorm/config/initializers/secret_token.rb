653cab9c2f3a3326a46416e832315bc915b587eb
drnkly <drn.kly@gmail.com>
2013-02-16 18:02:14 -0500

    Initial App Commit

---
+++ GeoStorm/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoStorm::Application.config.secret_token = 'ff74c0536efc743314ab46af9a0249c96c10441315044b9f9d57ccf9d8fd731be5270a7bb12086b1eb9129050730a01edb6717e8b3d7683ccd36832321a862c1'
